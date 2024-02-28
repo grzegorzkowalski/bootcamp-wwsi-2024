0d5d64bc5bfee2c99e71103c0b6f9c5588372a46
Grzegorz Kowalski <grzegorzkowalski@yahoo.com>
2024-02-28 21:36:39 +0100

    Chapter 2 Tasks 2,3

--- 02_Instrukcje_sterujace/Zadanie_03/main.js
+++ 02_Instrukcje_sterujace/Zadanie_03/main.js
@@ -1,1 +1,8 @@
 const cake = "🎂";
+const year = new Date().getFullYear();
+const birthday = 1986;
+const age = year - birthday;
+
+for (let i = 0; i < age; i++) {
+    console.log(i+1 + " - " + cake);
+}
